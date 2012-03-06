706f6fcaccb1bca0acf48dff2070875d66061322
Bertrand <sitcocolita@gmail.com>
2012-03-06 19:32:39 +0100

    Creation 1

---
+++ PAPPL/src/PageAccueil.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class PageAccueil extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PageAccueil frame = new PageAccueil();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PageAccueil() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		//Gestion du titre
+		JPanel haut = new JPanel();
+		JLabel Titre = new JLabel("Contrôle de présence");
+		Titre.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 17));
+		haut.add(Titre);
+		
+		//Gestion des trois boutons
+		JPanel milieux = new JPanel();
+		milieux.setLayout(new GridLayout(3,0));
+		JButton controler = new JButton("Commencer le contrôle de présence");
+		controler.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				setVisible(false);
+				SelectionCours selection = new SelectionCours();
+				selection.setVisible(true);
+			}
+		});
+		JPanel milieuxbas = new JPanel();
+		milieuxbas.setLayout(new FlowLayout());
+		
+		JButton majEtu = new JButton("Télécharger la liste des étudiants");
+		JButton majPhoto= new JButton("Télécharger les phtos des étudiants");
+
+		milieux.add(majEtu);
+		milieux.add(majPhoto);
+		milieux.add(controler);
+		milieux.add(majEtu);
+		milieux.add(majPhoto);
+				
+		
+		
+
+		contentPane.add(haut, BorderLayout.NORTH);
+		contentPane.add(milieux, BorderLayout.CENTER);
+
+	}
+	
+	
+
+}
---
+++ PAPPL/src/SelectionCours.java
@@ -0,0 +1,122 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+
+public class SelectionCours extends JFrame {
+
+	private JPanel contentPane;
+	private ArrayList<String> listeMatieres,listeGroupes;
+	
+	JComboBox<String> jcMatiere,jcGroupe;
+	
+	
+	/**
+	 * Create the frame.
+	 */
+	public SelectionCours() {
+		
+		listeMatieres = new ArrayList<String>();
+		listeMatieres.add("ANUME");
+		listeMatieres.add("SRETI");
+		listeMatieres.add("COPIN");
+		listeMatieres.add("GELOL");
+		listeMatieres.add("MESTR");
+		
+		listeGroupes = new ArrayList<String>();
+		listeGroupes.add("EI1_Gr1");
+		listeGroupes.add("EI1_Gr2");
+		listeGroupes.add("EI1_Gr3");
+		listeGroupes.add("EI1_Gr4");
+		listeGroupes.add("EI3_Info");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 349, 224);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		//Gestion du titre
+			JPanel haut = new JPanel();
+			JLabel Titre = new JLabel("Selection d\'un cours");
+			Titre.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 17));
+			haut.add(Titre);
+
+		//Gestion du milieu
+			JPanel milieux = new JPanel();
+			milieux.setLayout(new GridLayout(3,0));
+			
+			//Gestion du milieu 1
+				JPanel milieux1 = new JPanel();
+				milieux1.setLayout(new FlowLayout());
+				JLabel jlMatiere = new JLabel("Sélectionnez la matière");
+				jcMatiere = new JComboBox<String>();
+				remplireComboMatiere();
+				milieux1.add(jlMatiere);
+				milieux1.add(jcMatiere);
+			milieux.add(milieux1);
+			
+			//Gestion du milieu 2
+				JPanel milieux2 = new JPanel();
+				milieux2.setLayout(new FlowLayout());
+				JLabel jlgroupe = new JLabel("Sélectionnez le groupe");
+				jcGroupe = new JComboBox<String>();
+				remplireComboGroupe();
+				milieux2.add(jlgroupe);
+				milieux2.add(jcGroupe);
+			
+			milieux.add(milieux2);
+			
+			//Gestion du milieu 3
+				JPanel milieux3 = new JPanel();
+				milieux3.setLayout(new FlowLayout());
+				JLabel jlDate1 = new JLabel("Date");
+				JLabel jlDate2 = new JLabel("1er Janvier 2034 - 02h00");
+				milieux3.add(jlDate1);
+				milieux3.add(jlDate2);
+			
+			milieux.add(milieux3);
+
+			JPanel bas = new JPanel();
+			bas.setLayout(new FlowLayout());
+			JButton validerChoix = new JButton("Valider");
+			JButton retour = new JButton("Retour");
+			retour.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					setVisible(false);
+				}
+			});
+			bas.add(retour);
+			bas.add(validerChoix);
+
+		contentPane.add(haut, BorderLayout.NORTH);
+		contentPane.add(milieux, BorderLayout.CENTER);
+		contentPane.add(bas,BorderLayout.SOUTH);
+
+	}
+
+	public void remplireComboMatiere(){
+		for(String s : listeMatieres){
+			jcMatiere.addItem(s);
+		}
+	}
+	public void remplireComboGroupe(){
+		for(String s : listeGroupes){
+			jcGroupe.addItem(s);
+		}
+	}
+}
